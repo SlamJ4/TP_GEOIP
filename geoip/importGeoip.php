87f4c5e5c1404f329cf2a77b47e982d668df3914
SlamJ4 <98464327+SlamJ4@users.noreply.github.com>
2023-01-04 14:11:09 +0100

    Ajout commentaires importGeoip.php

--- geoip/importGeoip.php
+++ geoip/importGeoip.php
@@ -1,15 +1,18 @@
 <?php
+// Variables pour la connexion à la base
 $bdd_host = "localhost";
 $bdd_name = "tp_geoip";
 $bdd_table = "geoip";
 $bdd_login = "jordan";
 $bdd_password = "Ytreza4321";
 
+// Variables pour infos nécessaires à la lecture du fichier csv
 $csv_file = "geoip.csv";
 $terminated_by = ",";
 $enclosed_by = '"';
 $lines_terminated = "\r\n";
 
+// Connexion à la BDD
 try {
 	$bdd = new PDO("mysql:host=$bdd_host;dbname=$bdd_name;charset=utf8", $bdd_login, $bdd_password,
 	array(PDO::MYSQL_ATTR_LOCAL_INFILE => true)	
@@ -19,6 +22,7 @@ try {
 	error_log("Impossible de se connecter à la base !");
 }
 
+// Import des données du fichier dans la base
 try {
 $bdd -> exec("LOAD DATA LOCAL INFILE"
 	. $bdd -> quote($csv_file)
